1873b9aad22a3d8241ec5e9d3c318c1ef002a4b0
Leonardo <mr_kunkel@hotmail.com>
2026-04-27 14:38:01 -0600

    index, layout &  theme.ts changed

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,13 +1,22 @@
-import { Slot } from 'expo-router'
-import React from 'react'
-import { Text, View } from 'react-native'
+import { Colors } from '@/constants/theme';
+import { useFonts } from 'expo-font';
+import { Slot } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { View } from 'react-native';
 
 const RootLayout = () => {
-  return (
-    <View>
-      <Text>RootLayout</Text>
+  const [loaded] = useFonts({
+    SpaceMono: require('../../assets/fonts/SpaceMono-Regular.ttf')
+  });
 
-      <Slot></Slot>
+  if (!loaded) {
+    return null;
+  }
+  return (
+    <View style={{ backgroundColor: Colors.background, flex: 1 }}>
+      <Slot />
+
+      <StatusBar style='light' />
     </View>
   )
 }
