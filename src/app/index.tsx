66941465897f4efc8cf452e766f3a7f7fc331758
Leonardo <mr_kunkel@hotmail.com>
2026-04-28 12:30:06 -0600

    buttons with styles created

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,6 @@
+import CalculatorButton from '@/components/CalculatorButton'
 import ThemeText from '@/components/ThemeText'
+import { Colors } from '@/constants/theme'
 import { globalStyles } from '@/styles/global-styles'
 import { View } from 'react-native'
 
@@ -7,13 +9,26 @@ const CalculatorApp = () => {
   return (
     <View style={globalStyles.calculatorContainer}>
 
-      <ThemeText variant='h1'>
-        50 x 50
-      </ThemeText>
+      {/* Resultados */}
+      <View style={{ paddingHorizontal: 30, paddingBottom: 20 }}>
+        <ThemeText variant='h1'>
+          50 x 50
+        </ThemeText>
 
-      <ThemeText variant='h2'>
-        250
-      </ThemeText>
+        <ThemeText variant='h2'>
+          250
+        </ThemeText>
+      </View>
+
+      {/* Filas de Botones */}
+      <View style={globalStyles.row}>
+        {/* Botón 1 */}
+        <CalculatorButton label='C' blackText color={Colors.lightGray} onPress={() => console.log('C')} />
+        <CalculatorButton label='+/-' blackText color={Colors.lightGray} onPress={() => console.log('+/-')} />
+        <CalculatorButton label='del' blackText color={Colors.lightGray} onPress={() => console.log('del')} />
+        <CalculatorButton label='' onPress={() => console.log('')} />
+
+      </View>
 
     </View>
   )
