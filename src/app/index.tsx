d8201fc750f421691d7df9254a3b949dfc5bc3ed
Leonardo <mr_kunkel@hotmail.com>
2026-04-28 23:36:45 -0600

    validating the buttons logic

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,16 @@
 import CalculatorButton from '@/components/CalculatorButton'
 import ThemeText from '@/components/ThemeText'
 import { Colors } from '@/constants/theme'
+import { useCalculator } from '@/hooks/useCalculator'
 import { globalStyles } from '@/styles/global-styles'
 import { View } from 'react-native'
 
 const CalculatorApp = () => {
+
+  const {
+    formula,
+    buildNumber
+  } = useCalculator();
 
   return (
     <View style={globalStyles.calculatorContainer}>
@@ -12,7 +18,7 @@ const CalculatorApp = () => {
       {/* Resultados */}
       <View style={{ paddingHorizontal: 30, paddingBottom: 20 }}>
         <ThemeText variant='h1'>
-          50 x 50
+          {formula}
         </ThemeText>
 
         <ThemeText variant='h2'>
@@ -32,35 +38,35 @@ const CalculatorApp = () => {
 
       <View style={globalStyles.row}>
 
-        <CalculatorButton label='7' onPress={() => console.log('7')} />
-        <CalculatorButton label='8' onPress={() => console.log('8')} />
-        <CalculatorButton label='9' onPress={() => console.log('9')} />
+        <CalculatorButton label='7' onPress={() => buildNumber('7')} />
+        <CalculatorButton label='8' onPress={() => buildNumber('8')} />
+        <CalculatorButton label='9' onPress={() => buildNumber('9')} />
         <CalculatorButton label='X' color={Colors.orange} onPress={() => console.log('X')} />
 
       </View>
 
       <View style={globalStyles.row}>
 
-        <CalculatorButton label='4' onPress={() => console.log('4')} />
-        <CalculatorButton label='5' onPress={() => console.log('5')} />
-        <CalculatorButton label='6' onPress={() => console.log('6')} />
+        <CalculatorButton label='4' onPress={() => buildNumber('4')} />
+        <CalculatorButton label='5' onPress={() => buildNumber('5')} />
+        <CalculatorButton label='6' onPress={() => buildNumber('6')} />
         <CalculatorButton label='-' color={Colors.orange} onPress={() => console.log('-')} />
 
       </View>
 
       <View style={globalStyles.row}>
 
-        <CalculatorButton label='1' onPress={() => console.log('1')} />
-        <CalculatorButton label='2' onPress={() => console.log('2')} />
-        <CalculatorButton label='3' onPress={() => console.log('3')} />
+        <CalculatorButton label='1' onPress={() => buildNumber('1')} />
+        <CalculatorButton label='2' onPress={() => buildNumber('2')} />
+        <CalculatorButton label='3' onPress={() => buildNumber('3')} />
         <CalculatorButton label='X' color={Colors.orange} onPress={() => console.log('+')} />
 
       </View>
 
       <View style={globalStyles.row}>
 
-        <CalculatorButton label='0' doubleSize onPress={() => console.log('0')} />
-        <CalculatorButton label='.' onPress={() => console.log('.')} />
+        <CalculatorButton label='0' doubleSize onPress={() => buildNumber('0')} />
+        <CalculatorButton label='.' onPress={() => buildNumber('.')} />
         <CalculatorButton label='=' color={Colors.orange} onPress={() => console.log('=')} />
 
       </View>
---
+++ src/hooks/useCalculator.tsx
@@ -0,0 +1,35 @@
+import { useRef, useState } from "react";
+
+enum Operator {
+    add = '+',
+    subtract = '-',
+    multiply = 'x',
+    divide = '÷'
+}
+
+export const useCalculator = () => {
+
+    const [formula, setFormula] = useState('')
+
+    const [number, setNumber] = useState('0');
+    const [prevNumber, setPrevNumber] = useState('0');
+
+    const lastOperation = useRef<Operator | undefined>(undefined);
+
+    const buildNumber = (numberString: string) => {
+        console.log({ numberString });
+
+    }
+
+    return {
+        // Props
+        formula,
+        number,
+        prevNumber,
+
+        // Methods
+        buildNumber
+    }
+
+}
+
